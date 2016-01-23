46e460d55d198af5854723c8ec8225924bfde8e7
Nico Ameghino <nicolas.ameghino@gmail.com>
2016-01-22 22:11:47 -0300

    Initial Commit

---
+++ MapaTrenes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapaTrenes
+//
+//  Created by Nicolas Ameghino on 1/22/16.
+//  Copyright © 2016 Nicolas Ameghino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
